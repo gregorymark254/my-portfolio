cf39c049717684e29b8dbf0366cf299ded279acb
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-05 07:40:09 +0300

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -23,13 +23,13 @@ const About = () => {
           <h2 className='about__role'>A {role}.</h2>}
           <p className='about__desc'>{description && description}</p>
           <div className='about__contact center'>
-//             {resume && (
-//               <a href={resume}>
-//                 <span type='button' className='btn btn--outline'>
-//                   Resume
-//                 </span>
-//               </a>
-//             )}
+            {resume && (
+              <a href={resume}>
+                <span type='button' className='btn btn--outline'>
+                  Resume
+                </span>
+              </a>
+            )}
 
             {social && (
               <>
